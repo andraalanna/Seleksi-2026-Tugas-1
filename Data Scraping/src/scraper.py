1f2f7d34cec03cdb7d36839287948113e27f2a42
Andra <angelinaandra93@gmail.com>
2026-07-11 22:51:29 +0700

    feat: parse complicated header

--- Data Scraping/src/scraper.py
+++ Data Scraping/src/scraper.py
@@ -2,6 +2,7 @@ import cloudscraper
 from bs4 import BeautifulSoup
 import time
 import json
+from pprint import pprint
 
 headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) (Basis Data/Angelina Andra Alanna/13524079@std.stei.itb.ac.id)"}
 
@@ -42,37 +43,41 @@ def parse_table_raw(soup):
         all_rows.append(row_data) 
     return all_rows
 
-def map_row(header,row):
-    return dict(zip(header, row))
+def parse_headers(table):
+    header_rows = [row for row in table.find_all('tr') if row.find('th')]
+    tot_col = 0
+    for th in header_rows[0].find_all('th'):
+        tot_col += int(th.get('colspan', 1))
+    grid = [[None] * tot_col for _ in range(len(header_rows))]
 
+    for r_idx, row in enumerate(header_rows):
+        c_idx = 0
+        for th in row.find_all('th'):
+            while c_idx < tot_col and grid[r_idx][c_idx] is not None:
+                c_idx +=1
+            text = th.get_text(strip=True)
+            colspan = int(th.get('colspan', 1))
+            rowspan = int(th.get('rowspan', 1))
+            for r_offset in range(rowspan):
+                for c_offset in range(colspan):
+                    if r_idx + r_offset < len(header_rows):
+                        grid[r_idx + r_offset][c_idx + c_offset] = text
+            c_idx += colspan
+    return grid
+
+                    
 if __name__ == "__main__":
-    test_url = "https://ck3.paradoxwikis.com/List_of_hegemonies"
-    soup = fetch(test_url)
-    if soup:
-        print("Sukses! Otw Print Data Per Row")
-        raw_rows = parse_table_raw(soup)
-        # for row in raw_rows:
-        #     for i, value in enumerate(row):
-        #         print(i, value)
-        #     print('---------------------------------------------')
-        # print(f"Jumlah baris: {len(raw_rows)}")
-        header_row = raw_rows[0]
-        data_rows = raw_rows[1:]
-        header_clean  = [h.lower().replace(' ', '_') for h in header_row]
-        data = []
-        for row in data_rows:
-            data.append(map_row(header_clean, row))
-        
-        for d in data:
-            print(d)
-            
-        output_path = "Data Scraping/data/hegemonies.json"
-        
-        with open(output_path, "w", encoding="utf-8") as f:
-            json.dump(data, f, ensure_ascii=False, indent=4)
-            
-        print(f"\nSukses! Data berhasil disimpan ke: {output_path}")
-
-    else:
-        print("gagal")
-    
+    wesbite_to_scrape = [
+        {"url": "https://ck3.paradoxwikis.com/List_of_hegemonies", "name": "hegemonies"},
+        {"url": "https://ck3.paradoxwikis.com/List_of_duchies", "name": "duchies"},
+        {"url": "https://ck3.paradoxwikis.com/List_of_counties", "name": "counties"},
+        {"url": "https://ck3.paradoxwikis.com/List_of_kingdoms", "name": "kingdoms"},
+        {"url": "https://ck3.paradoxwikis.com/List_of_empires", "name": "empires"},
+    ]
+    for web in wesbite_to_scrape:
+        soup = fetch(web['url'])
+        if soup:
+            print("Sukses! Otw Print Data Per Row")
+            table = soup.find('table', class_='wikitable')
+            grid = parse_headers(table)
+            pprint(grid)
